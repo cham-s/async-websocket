984f47b9eacad390996d8270ffbf25c7dfc5d1a6
cham-s <cham-s@users.noreply.github.com>
2024-11-03 15:36:44 +0100

    add license

--- Sources/AsyncWebSocket/AsyncWebSocket.swift
+++ Sources/AsyncWebSocket/AsyncWebSocket.swift
@@ -1,5 +1,6 @@
 @_exported import AsyncWebSocketClient
 @_exported import AsyncWebSocketClientLive
+@_exported import AsyncWebSocketOperators
 @_exported import Dependencies
 @_exported import NIOCore
 @_exported import NIOPosix
