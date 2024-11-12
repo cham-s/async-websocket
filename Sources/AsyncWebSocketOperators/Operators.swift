5d83a8f03d2f9b271fdbf424a6136666407f6466
cham-s <cham-s@users.noreply.github.com>
2024-11-12 18:03:46 +0100

    Add validJSON operator

--- Sources/AsyncWebSocketOperators/Operators.swift
+++ Sources/AsyncWebSocketOperators/Operators.swift
@@ -17,6 +17,7 @@ extension AsyncStream where Element == AsyncWebSocketClient.Frame {
   ///  - Parameters:
   ///   - status: A CaseKeyPath for accessing the desired frame.
   ///   - onClose: A closure to invoke uppon close..
+  @inlinable
   public func on<Value>(
     _ frame: CaseKeyPath<AsyncWebSocketClient.Frame, Value>,
     onClose: (@Sendable (WebSocketErrorCode) async -> Void)? = nil
@@ -40,6 +41,7 @@ extension AsyncStream where Element == AsyncWebSocketClient.ConnectionStatus {
   ///   - status: A CaseKeyPath for accessing the desired status.
   ///   - onClose: A closure to invoke uppon close..
   ///   - onDidFail: A closure to invoke uppon failure..
+  @inlinable
   public func on<Value>(
     _ status: CaseKeyPath<AsyncWebSocketClient.ConnectionStatus, Value>,
     onDidClose: (@Sendable (WebSocketErrorCode) async -> Void)? = nil,
@@ -64,6 +66,7 @@ extension AsyncStream where Element: Sendable {
   /// Transforms a stream of Element into a stream of Result of transformed Element or Error.
   ///
   /// It allows the stream to continue even upon failure when attempting to transform the element.
+  @inlinable
   public func toResult<T: Sendable>(
     transform: @Sendable @escaping (Element) throws -> T
   ) -> AsyncStream<Result<T, any Error>> {
@@ -73,10 +76,29 @@ extension AsyncStream where Element: Sendable {
       }
       .eraseToStream()
   }
+  
+  public func resultCatchingFailure<T: Sendable>(
+    transform: @Sendable @escaping (Element) throws -> T,
+    operation: (@Sendable (any Error) -> Void )? = nil
+  ) -> AsyncStream<T> {
+    self
+      .toResult(transform: transform)
+      .compactMap {
+        switch $0 {
+        case let .success(success):
+          return success
+        case let .failure(error):
+          operation?(error)
+          return nil
+        }
+      }
+      .eraseToStream()
+  }
 }
 
 extension AsyncStream where Element == AsyncWebSocketClient.Frame {
   /// Attemps to decode into JSON if the frame is message.data or message.text.
+  @inlinable
   public func json<T>(
     _ type: T.Type
   ) -> AsyncStream<Result<T, any Error>> where T: Codable, T: Sendable {
@@ -97,5 +119,29 @@ extension AsyncStream where Element == AsyncWebSocketClient.Frame {
           return  try JSONDecoder().decode(T.self, from: data)
         }
       }
+  }
+  
+  /// Decodes incoming text and data frames into JSON ignoring failing conversion to JSON.
+  @inlinable
+  public func validJSON<T>(
+    _ type: T.Type
+  ) -> AsyncStream<T> where T: Codable, T: Sendable {
+    self
+      .on(\.message)
+      .resultCatchingFailure(transform: {
+        switch $0 {
+        case let .binary(data):
+          return try JSONDecoder().decode(T.self, from: data)
+        case let .text(string):
+          guard let data = string.data(using: .utf8) else {
+            throw NSError(
+              domain: "String To Data Conversion",
+              code: 0,
+              userInfo: ["reason": "Faield to convert string to data"]
+            )
+          }
+          return  try JSONDecoder().decode(T.self, from: data)
+        }
+      })
   }
 }
